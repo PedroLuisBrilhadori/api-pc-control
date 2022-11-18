c6c42117efaf367d3526d57f3e14fc3a979dad53
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2022-11-18 16:22:46 -0300

    feat: adiciona middleware de autenticação

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
 
 export async function validateSanitizedRequest(req: Request, res: Response, next: NextFunction) {
   const errors = validationResult(req);
@@ -9,4 +10,22 @@ export async function validateSanitizedRequest(req: Request, res: Response, next
   } else {
     next();
   }
+}
+
+export function verifyToken(req: Request, res: Response, next: NextFunction) {
+  const token = req.body.token || req.query.token || req.headers["x-access-token"];
+
+  if (!token) {
+    return res.status(403).send({ success: false, message: "É necessário um token para autenticação" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_TOKEN);
+    req.body.user = decoded;
+  } catch (error) {
+    console.error(error);
+    return res.status(401).send({ success: false, message: "Token inválido." });
+  }
+
+  return next();
 }
